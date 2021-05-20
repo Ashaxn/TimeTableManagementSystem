5237ea8f9a6da4be72b2d078b7ab1d3a3ee38db7
hershey99 <it19102610@my.sliit.lk>
2021-05-20 23:01:10 +0530

    Generate timetable

---
+++ src/timetablemanagementsystem/GenerateTimeModel.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timetablemanagementsystem;
+
+/**
+ *
+ * @author user
+ */
+public class GenerateTimeModel {
+    
+    private String timeslot;
+    private String monday,tuesday,wednesday,thrusday,friday,saterday,sunday;
+    
+    
+    public String gettimeslot() {
+        return timeslot;
+    }
+
+    public void settimeslot(String timeslot) {
+        this.timeslot = timeslot;
+    }
+    
+    public String getMonday() {
+        return monday;
+    }
+
+    public void setMonday(String monday) {
+        this.monday = monday;
+    }
+
+    public String getTuesday() {
+        return tuesday;
+    }
+
+    public void setTuesday(String tuesday) {
+        this.tuesday = tuesday;
+    }
+
+    public String getWednesday() {
+        return wednesday;
+    }
+
+    public void setWednesday(String wednesday) {
+        this.wednesday = wednesday;
+    }
+
+    public String getThrusday() {
+        return thrusday;
+    }
+
+    public void setThrusday(String thrusday) {
+        this.thrusday = thrusday;
+    }
+
+    public String getFriday() {
+        return friday;
+    }
+
+    public void setFriday(String friday) {
+        this.friday = friday;
+    }
+
+    public String getSaterday() {
+        return saterday;
+    }
+
+    public void setSaterday(String saterday) {
+        this.saterday = saterday;
+    }
+
+    public String getSunday() {
+        return sunday;
+    }
+
+    public void setSunday(String sunday) {
+        this.sunday = sunday;
+    }
+    
+}
